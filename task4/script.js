1b0fbdd6db51b7a567a5c37eb45c2bfdf5da8cda
bvs3040 <taga-si@mail.ru>
2021-12-30 15:12:38 +0700

    prevent default

--- task4/script.js
+++ task4/script.js
@@ -1,10 +1,12 @@
 const textA=document.querySelector('a');
 
-textA.addEventListener('click', function(){
-    this.textContent=prompt('Введите новый текст ссылки');
-    while (textA.textContent=="") {
+textA.addEventListener('click', function(event){
+    event.preventDefault();
+    this.textContent=prompt('Введите новый текст ссылки', "введите текст");
+        while (textA.textContent==('')) {
         this.textContent=prompt('Текст нужно ввести');
-    }    
+    } 
+    
 })
 
  
