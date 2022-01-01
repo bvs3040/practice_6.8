2b117110b8b205713679b812c3d982762d268a5e
bvs3040 <taga-si@mail.ru>
2022-01-01 23:47:45 +0700

    task 3 mod

--- task3/script.js
+++ task3/script.js
@@ -1,24 +1,65 @@
-const trafficLightEI=document.querySelector('#trafficLight');
+const trafficLightEI1=document.querySelector('#trafficLight1');
+const trafficLightEI2=document.querySelector('#trafficLight2');
+const trafficLightEI3=document.querySelector('#trafficLight3');
 
-function makeGreen() {
-    trafficLightEI.style.background=('green');
-    trafficLightEI.removeEventListener('click', makeGreen);
-    trafficLightEI.addEventListener('click', makeYellow);
-}
-trafficLightEI.addEventListener('click', makeYellow);
+function makeGreen1() {
+    trafficLightEI1.style.background=('green');
+    trafficLightEI2.style.background=('black');
+    trafficLightEI3.style.background=('black');
 
-function makeYellow() {
-    trafficLightEI.style.background=('yellow');
-    trafficLightEI.removeEventListener('click', makeYellow);
-    trafficLightEI.addEventListener('click', makeRed);
-}
-trafficLightEI.addEventListener('click', makeRed);
+    trafficLightEI1.addEventListener('click', makeGreen1);
+    trafficLightEI2.addEventListener('click', makeGreen1);
+    trafficLightEI3.addEventListener('click', makeGreen1);
 
-function makeRed() {
-    trafficLightEI.style.background=('red');
-    trafficLightEI.removeEventListener('click', makeRed);
-    trafficLightEI.addEventListener('click', makeGreen);
-}
-trafficLightEI.addEventListener('click', makeGreen);
+    trafficLightEI1.removeEventListener('click', makeGreen1);
+    trafficLightEI2.removeEventListener('click', makeGreen1);
+    trafficLightEI3.removeEventListener('click', makeGreen1);
+
+    trafficLightEI2.addEventListener('click', makeYellow2);
+    trafficLightEI3.addEventListener('click', makeYellow2);
+    trafficLightEI1.addEventListener('click', makeYellow2);
+   }
+
+
+function makeYellow2() {
+    trafficLightEI2.style.background=('yellow');
+    trafficLightEI1.style.background=('black');
+    trafficLightEI3.style.background=('black');
+
+    trafficLightEI2.addEventListener('click', makeYellow2);
+    trafficLightEI3.addEventListener('click', makeYellow2);
+    trafficLightEI1.addEventListener('click', makeYellow2);
+
+    trafficLightEI2.removeEventListener('click', makeYellow2);
+    trafficLightEI1.removeEventListener('click', makeYellow2);
+    trafficLightEI3.removeEventListener('click', makeYellow2);
+
+    trafficLightEI3.addEventListener('click', makeRed3);
+    trafficLightEI2.addEventListener('click', makeRed3);
+    trafficLightEI1.addEventListener('click', makeRed3);
+  }
+
+
+function makeRed3() {
+    trafficLightEI3.style.background=('red');
+    trafficLightEI1.style.background=('black');
+    trafficLightEI2.style.background=('black');
+
+    trafficLightEI3.addEventListener('click', makeRed3);
+    trafficLightEI2.addEventListener('click', makeRed3);
+    trafficLightEI1.addEventListener('click', makeRed3);
+
+    trafficLightEI3.removeEventListener('click', makeRed3);
+    trafficLightEI2.removeEventListener('click', makeRed3);
+    trafficLightEI1.removeEventListener('click', makeRed3);
+    
+    trafficLightEI1.addEventListener('click', makeGreen1);
+    trafficLightEI2.addEventListener('click', makeGreen1);
+    trafficLightEI3.addEventListener('click', makeGreen1);
+  }
+
+trafficLightEI1.addEventListener('click', makeGreen1);
+trafficLightEI2.addEventListener('click', makeGreen1);
+trafficLightEI3.addEventListener('click', makeGreen1);
 
 
