59242aebb736cf9d54b0569f002eb38e865bd5fb
bvs3040 <taga-si@mail.ru>
2022-01-01 17:55:18 +0700

    console

---
+++ task5/script.js
@@ -0,0 +1,34 @@
+const form=document.querySelector('form');
+//const duplicateField=document.querySelector('#duplicateField');
+//const button=document.querySelector('button');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //const form=document.querySelector('form');
+
+    console.log(form.elements.userText.value);
+    form.reset();
+
+
+});
+
+
+
+
+
+    
+
+
+    
+
+    /*console.log(input);
+    console.log(duplicateField);
+    console.log(button);*/
+    
+
+
+
+
+
+
